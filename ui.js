42ef258c3f20bea074fd6ee7321e7dda7b4744ec
Tiago Costa <tiagocostadossantoscosta@gmail.com>
2025-11-18 14:47:53 -0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,17 +1,1 @@
-  // Carrinho
-  document.addEventListener("click", (e) => {
-    const dec = e.target.closest("[data-dec]");
-    const inc = e.target.closest("[data-inc]");
-    if (dec || inc) {
-      const wrap = e.target.closest(".qty");
-      const input = wrap.querySelector("input[type='number']");
-      let val = parseInt(input.value || "1", 10);
-      val = inc ? val + 1 : Math.max(1, val - 1);
-      input.value = val;
-    }
-    if (e.target.classList.contains("remove")) {
-      e.target.closest(".cart-item")?.remove();
-    }
-  });
-});
 
